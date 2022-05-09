caea9196abd2904b4b912e9e10490f3f78faaf19
Kozhemykin Vitaliy <vikozhemyakin@ozon.ru>
2022-05-09 13:37:36 +0300

    add NewDummyPhotoService

--- internal/service/photovideo/photo/service.go
+++ internal/service/photovideo/photo/service.go
@@ -2,6 +2,14 @@ package photo
 
 import "github.com/ozonmp/omp-bot/internal/model/photovideo"
 
+var allEntities = []photovideo.Photo{
+	{Id: 1, Name: "one"},
+	{Id: 2, Name: "two"},
+	{Id: 3, Name: "three"},
+	{Id: 4, Name: "four"},
+	{Id: 5, Name: "five"},
+}
+
 type PhotoService interface {
 	Describe(photoID uint64) (*photovideo.Photo, error)
 	List(cursor uint64, limit uint64) ([]photovideo.Photo, error)
@@ -10,8 +18,10 @@ type PhotoService interface {
 	Remove(photoID uint64) (bool, error)
 }
 
-type DummyPhotoService struct{}
+type DummyPhotoService struct {
+	allEntities []photovideo.Photo
+}
 
 func NewDummyPhotoService() *DummyPhotoService {
-	return &DummyPhotoService{}
+	return &DummyPhotoService{allEntities: allEntities}
 }
