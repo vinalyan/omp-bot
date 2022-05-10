cc560b98a1049c8c049a21983cf1f0c4e0d97700
Kozhemykin Vitaliy <vikozhemyakin@ozon.ru>
2022-05-10 06:50:02 +0300

    мелочь

--- internal/app/commands/photovideo/photo/commander.go
+++ internal/app/commands/photovideo/photo/commander.go
@@ -2,6 +2,7 @@ package photo
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	service "github.com/ozonmp/omp-bot/internal/service/photovideo/photo"
 )
 
 type PhotoCommander interface {
@@ -12,4 +13,12 @@ type PhotoCommander interface {
 
 	New(inputMsg *tgbotapi.Message)  // return error not implemented
 	Edit(inputMsg *tgbotapi.Message) // return error not implemented
+}
+
+type Commander struct {
+}
+
+func NewPhotoCommander(bot *tgbotapi.BotAPI, service service.PhotoService) Commander {
+	// ...
+	return Commander{}
 }
